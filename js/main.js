2ff2d11780ad110a953e3cc142a99e950f8f48fb
miguelgazela <miguel.gazela@gmail.com>
2014-03-11 20:59:35 +0000

    trying the navbar and jump to navigation proposed by the design team

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+function goToByScroll(id){
+    var scrollY = $(id).offset().top;
+    $('html,body').animate({scrollTop: scrollY},'slow');
+}
 
+$(document).ready(function() {
+    console.log('ready');
+    $('.navbar-nav a').click(function(){
+        console.log("works!");
+        goToByScroll($(this).attr('href'));
+        return false;
+    });
+});
